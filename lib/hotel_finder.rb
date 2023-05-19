be4033ed6ca533c885bb0e3621851b06d1e1a41a
Yasmin Valim <yasmin.valimds@gmail.com>
2023-05-19 15:33:30 +0100

    Feat: create methods to build URL

--- lib/hotel_finder.rb
+++ lib/hotel_finder.rb
@@ -2,7 +2,40 @@
 
 require_relative "hotel_finder/version"
 
+require 'uri'
+require 'net/http'
+
 module HotelFinder
-  class Error < StandardError; end
-  # Your code goes here...
+  class Finder
+    BOOKING_URL_BASE = 'https://www.booking.com'.freeze
+    HOLIDAYCHECK_URL_BASE = 'https://www.holidaycheck.de'.freeze
+    TRIPADVISOR_URL_BASE = 'http://www.tripadvisor.com'.freeze
+
+    def initialize(hotel_name)
+      @hotel_name = hotel_name
+    end
+
+    def match
+      urls = {}
+      urls[:booking] = booking_url
+      urls[:holidaycheck] = holidaycheck_url
+      urls[:tripadvisor] = tripadvisor_url
+    
+      urls
+    end
+
+    private
+
+    def tripadvisor_url
+      "#{TRIPADVISOR_URL_BASE}/Search?q=#{URI.encode_www_form_component(@hotel_name)}"
+    end
+
+    def booking_url
+      "#{BOOKING_URL_BASE}/searchresults.html?ss=#{URI.encode_www_form_component(@hotel_name)}"
+    end
+
+    def holidaycheck_url
+      "#{HOLIDAYCHECK_URL_BASE}/search?hotel=#{URI.encode_www_form_component(@hotel_name)}"
+    end
+  end
 end
