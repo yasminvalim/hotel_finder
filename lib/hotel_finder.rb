a89e25fd2c3991eb82938cd9b0c7aea38f051ff3
Yasmin Valim <yasmin.valimds@gmail.com>
2023-05-19 14:38:43 +0100

    feat: adjuting gemspec

---
+++ lib/hotel_finder.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hotel_finder/version"
+
+module HotelFinder
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hotel_finder/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module HotelFinder
+  VERSION = "0.1.0"
+end
